3f05dbd3afbb7d0aaa83bd8367f09a0a2dd9e70f
kralicek55 <116740012+kralicek55@users.noreply.github.com>
2023-05-13 13:26:02 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,79 +1,115 @@
+function CervenaAuto (sviti: number) {
+    pins.digitalWritePin(DigitalPin.P0, sviti)
+}
 function BlikejOranzovou () {
-    RozsvitOranzovou()
+    SmeaforChodce("nic")
+    SemaforAuta("oranzova")
     basic.pause(1000)
-    ZhasniOranzovou()
+    SemaforAuta("nic")
     basic.pause(1000)
+}
+function ZelenaAuto (sviti: number) {
+    pins.digitalWritePin(DigitalPin.P2, sviti)
+}
+function SemaforAuta (coMaSvitit: string) {
+    if (coMaSvitit == "cervena") {
+        CervenaAuto(1)
+        OranzovaAuto(0)
+        ZelenaAuto(0)
+    } else if (coMaSvitit == "cervenaOranzova") {
+        CervenaAuto(1)
+        OranzovaAuto(1)
+        ZelenaAuto(0)
+    } else if (coMaSvitit == "zelena") {
+        CervenaAuto(0)
+        OranzovaAuto(0)
+        ZelenaAuto(1)
+    } else if (coMaSvitit == "oranzova") {
+        CervenaAuto(0)
+        OranzovaAuto(1)
+        ZelenaAuto(0)
+    } else {
+        CervenaAuto(0)
+        OranzovaAuto(0)
+        ZelenaAuto(0)
+    }
 }
 function ZelenaChodec (sviti: number) {
-    if (sviti == 1) {
-        pins.digitalWritePin(DigitalPin.P8, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P8, 0)
-    }
+    pins.digitalWritePin(DigitalPin.P8, sviti)
 }
 function CervenaChodec (sviti: number) {
-    if (sviti == 1) {
-        pins.digitalWritePin(DigitalPin.P16, 1)
-    } else {
-        pins.digitalWritePin(DigitalPin.P16, 0)
-    }
+    pins.digitalWritePin(DigitalPin.P16, sviti)
 }
-function Chodec (coMaSvitit: string) {
-    if (coMaSvitit == "nic") {
-        CervenaChodec(0)
-        ZelenaChodec(0)
-    } else if (coMaSvitit == "cervena") {
-        CervenaChodec(1)
-        ZelenaChodec(0)
-    } else {
-        CervenaChodec(0)
-        ZelenaChodec(1)
+function OranzovaAuto (sviti: number) {
+    pins.digitalWritePin(DigitalPin.P1, sviti)
+}
+function Chodec () {
+    if (prepniChodec == 1) {
+        SmeaforChodce("cervena")
+        basic.pause(2000)
+        SemaforAuta("oranzova")
+        basic.pause(2000)
+        SemaforAuta("cervena")
+        basic.pause(2000)
+        SmeaforChodce("zelena")
+        prepniChodec = 0
+        basic.pause(5000)
+        prepniAuto = 1
+        mod = 2
     }
 }
 input.onButtonPressed(Button.A, function () {
-    mod = 0
+    if (mod == 1) {
+        prepniAuto = 1
+        mod = 2
+    }
 })
-function ZhasniCervenou () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-}
 function Auto () {
-    RozsvitCervenou()
-    basic.pause(5000)
-    RozsvitOranzovou()
-    basic.pause(2000)
-    ZhasniCervenou()
-    ZhasniOranzovou()
-    RozsvitZelenou()
+    if (prepniAuto == 1) {
+        SmeaforChodce("cervena")
+        SemaforAuta("cervena")
+        basic.pause(3000)
+        SemaforAuta("cervenaOranzova")
+        basic.pause(2000)
+        SemaforAuta("zelena")
+        prepniAuto = 0
+    }
 }
-function ZhasniOranzovou () {
-    pins.digitalWritePin(DigitalPin.P1, 0)
-}
-function RozsvitOranzovou () {
-    pins.digitalWritePin(DigitalPin.P1, 1)
-}
-function ZhasniZelenou () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
+function SmeaforChodce (coMaSvitit: string) {
+    if (coMaSvitit == "cervena") {
+        CervenaChodec(1)
+        ZelenaChodec(0)
+    } else if (coMaSvitit == "zelena") {
+        CervenaChodec(0)
+        ZelenaChodec(1)
+    } else {
+        CervenaChodec(0)
+        ZelenaChodec(0)
+    }
 }
 input.onButtonPressed(Button.B, function () {
-    mod = 2
+    if (mod == 2) {
+        mod = 1
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    mod = 1
+    if (mod == 2) {
+        mod = 3
+        prepniChodec += 1
+    }
 })
-function RozsvitZelenou () {
-    pins.digitalWritePin(DigitalPin.P2, 1)
-}
-function RozsvitCervenou () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-}
 // 0 - vypnutý
 // 1 - bliká oranžová
 // 2 - auto
 // 3 - chodec
 // 
 // 
+let prepniChodec = 0
+let prepniAuto = 0
 let mod = 0
 mod = 1
+prepniAuto += 0
+prepniChodec = 0
 basic.showLeds(`
     . . # # #
     . # . # #
@@ -87,6 +123,6 @@ basic.forever(function () {
     } else if (mod == 2) {
         Auto()
     } else {
-    	
+        Chodec()
     }
 })
